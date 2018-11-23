a11a3d2274a963ff1d12c2592e7d47ddfd26f7fc
xavier0078 <xavier.adell0078@gamil.com>
2018-11-23 18:16:41 +0100

    final del juego adivina un numero.

--- adivina_numero.py
+++ adivina_numero.py
@@ -10,20 +10,28 @@ if number_to_guess == user_number:
 else:
     print ("Has fallado intentalo otra vez")
     user_number = int(input("Adivina un numero: "))
-
     if number_to_guess == user_number:
         print("Has ganado")
         print("Fin del juego")
-    else:
-        print("Has fallado intentalo una ultima vez vez")
-        user_number = int(input("Adivina un numero: "))
 
+    else:
+        print("Has fallado intentalo otra vez")
+        user_number = int(input("Adivina un numero: "))
         if number_to_guess == user_number:
             print("Has ganado")
             print("Fin del juego")
         else:
-            print("Has perdido ")
-            print("Fin del juego")
+            print("Has fallado intentalo una ultima vez vez")
+            user_number = int(input("Adivina un numero: "))
+
+            if number_to_guess == user_number:
+                print("Has ganado")
+                print("Fin del juego")
+            else:
+                print("Has perdido ")
+                print("Fin del juego")
+
+
 
 
 
